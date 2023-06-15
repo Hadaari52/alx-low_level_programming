1c91c8c15e105d00b9703714b80fef1294a71bc4
Hadaari52 <83499828+Hadaari52@users.noreply.github.com>
2023-06-16 02:50:37 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-puts('"Programming is like building a multilingual puzzle,');
-return (0);
+printf("%s\n", "Programming is like building a multilingual puzzle,");
+return 0;
 }
