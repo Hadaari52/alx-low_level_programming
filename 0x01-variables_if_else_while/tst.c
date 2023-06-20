1de108a10802aca5f3b17fdeb7471e1023eb7b00
Hadaari52 <mhadaari52@gmail.com>
2023-06-21 00:36:01 +0300

    First version

---
+++ 0x01-variables_if_else_while/tst.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+printf("Hello world");
+return (0);
+}
